7648eaa22bcd67547f25a1ddff0b3cd2ed451729
Daniel Gultsch <daniel@gultsch.de>
2017-12-31 13:15:53 +0100

    mod_omemo_all_access: fixed some linter warnings

--- mod_omemo_all_access/mod_omemo_all_access.lua
+++ mod_omemo_all_access/mod_omemo_all_access.lua
@@ -27,7 +27,7 @@ local function on_pep_request(event)
 			local item = payload.tags[1];
 			if item and item.name == 'item' then
 				requested_id = item.attr.id;
-			end 
+			end
 		end
 		if node and string.sub(node,1,string.len(white_listed_namespace)) == white_listed_namespace then
 			local user = stanza.attr.to and jid_bare(stanza.attr.to) or session.username..'@'..session.host;
@@ -35,13 +35,13 @@ local function on_pep_request(event)
 			if user_data and user_data[node] then
 				local id, item = unpack(user_data[node]);
 				if not requested_id or id == requested_id then
-					local stanza = st.reply(stanza)
+					local reply_stanza = st.reply(stanza)
 						:tag('pubsub', {xmlns='http://jabber.org/protocol/pubsub'})
 							:tag('items', {node=node})
 								:add_child(item)
 							:up()
 						:up();
-					session.send(stanza);
+					session.send(reply_stanza);
 					module:log("debug","provided access to omemo node",node)
 					return true;
 				end
