d4311b7e7ea4e6659cbe58a8838fad5511dac17e
Matthew Wild <mwild1@gmail.com>
2018-07-02 12:56:51 +0100

    mod_http_upload_external: Log file type and size

--- mod_http_upload_external/mod_http_upload_external.lua
+++ mod_http_upload_external/mod_http_upload_external.lua
@@ -83,7 +83,7 @@ local function handle_request(origin, stanza, xmlns, filename, filesize, filetyp
 	local get_url, verify = magic_crypto_dust(random, filename, filesize, filetype);
 	local put_url = get_url .. verify;
 
-	module:log("info", "Handing out upload slot %s to %s@%s", get_url, origin.username, origin.host);
+	module:log("info", "Handing out upload slot %s to %s@%s [%d %s]", get_url, origin.username, origin.host, filesize, filetype);
 
 	return get_url, put_url;
 end
