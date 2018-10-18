822570a6c4654aaac998fe8cf11e440374958ed4
Kim Alvefur <zash@zash.se>
2018-10-18 15:41:52 +0200

    mod_log_json: JSON log sink

---
+++ mod_log_json/mod_log_json.lua
@@ -0,0 +1,32 @@
+local pack = require "util.table".pack;
+local json = require "util.json";
+local array = require "util.array";
+local datetime = require "util.datetime".datetime;
+
+module:set_global();
+
+local function sink_maker(config)
+	local logfile = io.open("blah.json", "a");
+	logfile:setvbuf("no");
+	return function (source, level, message, ...)
+		local args = pack(...);
+		for i = 1, args.n do
+			if args[i] == nil then
+				args[i] = json.null;
+			elseif type(args[i]) ~= "string" or type(args[i]) ~= "number" then
+				args[i] = tostring(args[i]);
+			end
+		end
+		args.n = nil;
+		local payload = {
+			datetime = datetime(),
+			source = source,
+			level = level,
+			message = message,
+			args = array(args);
+		};
+		logfile:write(json.encode(payload), "\n");
+	end
+end
+
+require"core.loggingmanager".register_sink_type("json", sink_maker);
