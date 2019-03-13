73708075eddc2192bf0e47cd299a78add1cf7b57
Kim Alvefur <zash@zash.se>
2019-03-13 22:47:14 +0100

    mod_http_muc_log: Split long line [luacheck]

--- mod_http_muc_log/mod_http_muc_log.lua
+++ mod_http_muc_log/mod_http_muc_log.lua
@@ -40,7 +40,9 @@ do
 	end
 	if not template then
 		module:log("error", "Error loading template: %s", err);
-		template = render("<h1>mod_{module} could not read the template</h1><p>Tried to open <b>{filename}</b></p><pre>{error}</pre>",
+		template = render("<h1>mod_{module} could not read the template</h1>\
+		<p>Tried to open <b>{filename}</b></p>\
+		<pre>{error}</pre>",
 			{ module = module.name, filename = template_filename, error = err });
 	end
 end
