307e627f6f547fc24b3c3ec273ad44f776d1ff78
JC Brand <jc@opkode.com>
2019-08-02 07:44:11 +0200

    mod_muc_block_pm: Add support for Prosody 0.11

--- mod_muc_block_pm/mod_muc_block_pm.lua
+++ mod_muc_block_pm/mod_muc_block_pm.lua
@@ -1,12 +1,18 @@
 local bare_jid = require"util.jid".bare;
 local st = require"util.stanza";
 
-local muc_rooms = module:depends"muc".rooms;
+-- Support both old and new MUC code
+local mod_muc = module:depends"muc";
+local rooms = rawget(mod_muc, "rooms");
+local get_room_from_jid = rawget(mod_muc, "get_room_from_jid") or
+	function (jid)
+		return rooms[jid];
+	end
 
 module:hook("message/full", function(event)
 	local stanza, origin = event.stanza, event.origin;
 	local to, from = stanza.attr.to, stanza.attr.from;
-	local room = muc_rooms[bare_jid(to)];
+	local room = get_room_from_jid(bare_jid(to));
 	local to_occupant = room and room._occupants[to];
 	local from_occupant = room and room._occupants[room._jid_nick[from]]
 	if not ( to_occupant and from_occupant ) then return end
