18ea65100a3cea24577cc747a163d27f6b8e90f6
Kim Alvefur <zash@zash.se>
2019-08-09 18:59:35 +0200

    mod_turncredentials: Add parallel implementation of XEP-0215 v0.7

--- mod_turncredentials/mod_turncredentials.lua
+++ mod_turncredentials/mod_turncredentials.lua
@@ -6,6 +6,7 @@ local st = require "util.stanza";
 local hmac_sha1 = require "util.hashes".hmac_sha1;
 local base64 = require "util.encodings".base64;
 local os_time = os.time;
+local datetime = require "util.datetime".datetime;
 local secret = module:get_option_string("turncredentials_secret");
 local host = module:get_option_string("turncredentials_host"); -- use ip addresses here to avoid further dns lookup latency
 local port = module:get_option_number("turncredentials_port", 3478);
@@ -28,6 +29,23 @@ module:hook("iq-get/host/urn:xmpp:extdisco:1:services", function(event)
     origin.send(st.reply(stanza):tag("services", {xmlns = "urn:xmpp:extdisco:1"})
         :tag("service", { type = "stun", host = host, port = ("%d"):format(port) }):up()
         :tag("service", { type = "turn", host = host, port = ("%d"):format(port), username = userpart, password = nonce, ttl = ("%d"):format(ttl) }):up()
+    );
+    return true;
+end);
+
+module:add_feature("urn:xmpp:extdisco:2");
+
+module:hook("iq-get/host/urn:xmpp:extdisco:2:services", function(event)
+    local origin, stanza = event.origin, event.stanza;
+    if origin.type ~= "c2s" then
+        return;
+    end
+    local now = os_time() + ttl;
+    local userpart = tostring(now);
+    local nonce = base64.encode(hmac_sha1(secret, tostring(userpart), false));
+    origin.send(st.reply(stanza):tag("services", {xmlns = "urn:xmpp:extdisco:2"})
+        :tag("service", { type = "stun", host = host, port = ("%d"):format(port) }):up()
+        :tag("service", { type = "turn", host = host, port = ("%d"):format(port), username = userpart, password = nonce, expires = datetime(ttl), restricted = "1"  }):up()
     );
     return true;
 end);
