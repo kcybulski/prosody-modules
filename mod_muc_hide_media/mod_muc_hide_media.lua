6f87b63a34884e8d1129d8ff375b79d848e2440c
Matthew Wild <mwild1@gmail.com>
2019-09-29 18:53:00 +0100

    mod_muc_hide_media: Also strip SIMS media

--- mod_muc_hide_media/mod_muc_hide_media.lua
+++ mod_muc_hide_media/mod_muc_hide_media.lua
@@ -47,11 +47,22 @@ module:hook("muc-disco#info", function (event)
 	formdata["{xmpp:prosody.im}muc#roomconfig_display_media"] = display_media;
 end);
 
+local function filter_media_tags(tag)
+	local xmlns = tag.attr.xmlns;
+	if xmlns == "jabber:x:oob" then
+		return nil;
+	elseif xmlns == "urn:xmpp:reference:0" then
+		if tag:get_child("media-sharing", "urn:xmpp:sims:1") then
+			return nil;
+		end
+	end
+	return tag;
+end
 
 module:hook("muc-occupant-groupchat", function (event)
 	local stanza = event.stanza;
 	if stanza.attr.type ~= "groupchat" then return; end
 	if should_hide_media(event.room) then
-		stanza:remove_children("x", "jabber:x:oob");
+		stanza:maptags(filter_media_tags);
 	end
 end, 20);
