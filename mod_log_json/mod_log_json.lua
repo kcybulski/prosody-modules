7cf50913eaace2bf73f472163f3e7cfa2a9288d6
Kim Alvefur <zash@zash.se>
2019-11-13 17:54:54 +0100

    mod_log_json: Fix to include underscore in UDP details in the other spot

--- mod_log_json/mod_log_json.lua
+++ mod_log_json/mod_log_json.lua
@@ -16,7 +16,7 @@ local function sink_maker(config)
 		end
 	elseif config.udp_host and config.udp_port then
 		local conn = socket.udp();
-		conn:connect(config.udphost, config.udpport);
+		conn:connect(config.udp_host, config.udp_port);
 		function send(payload)
 			conn:send(payload);
 		end
