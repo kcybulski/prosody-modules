e39449df246963cdc5a879734611787fda5057b7
Kim Alvefur <zash@zash.se>
2020-01-08 21:02:47 +0100

    mod_stats39: Use a more local reference (silence luacheck warning)

--- mod_stats39/mod_stats39.lua
+++ mod_stats39/mod_stats39.lua
@@ -4,7 +4,7 @@ local st = require "util.stanza";
 module:hook("iq/host/http://jabber.org/protocol/stats:query", function (event)
 	local origin, stanza = event.origin, event.stanza;
 	local stats, _, extra = statsman.get_stats();
-	local reply = st.reply(event.stanza);
+	local reply = st.reply(stanza);
 	reply:tag("query", { xmlns = "http://jabber.org/protocol/stats" });
 	for stat, value in pairs(stats) do
 		local unit = extra[stat] and extra[stat].units;
