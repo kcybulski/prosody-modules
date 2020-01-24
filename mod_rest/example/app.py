fe9b57e38ab75e0ca52d208b12586be23eef5974
Kim Alvefur <zash@zash.se>
2020-01-25 00:37:06 +0100

    mod_rest: Add an examlpe Flask app

---
+++ mod_rest/example/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, Response, request, jsonify
+
+app = Flask("echobot")
+
+
+@app.route("/api", methods=["POST"])
+def hello():
+    print(request.data)
+    if request.is_json:
+        data = request.get_json()
+
+        if "kind" not in data:
+            return Response(status=400)
+
+        if data["kind"] == "message" and "body" in data:
+            return jsonify({"body": "Yes this is flask app"})
+
+        elif data["kind"] == "iq" and data["type"] == "get":
+            if "ping" in data:
+                return Response(status=204)
+
+            elif "disco" in data:
+                return jsonify(
+                    {
+                        "disco": {
+                            "identities": [
+                                {
+                                    "category": "component",
+                                    "type": "generic",
+                                    "name": "Flask app",
+                                }
+                            ],
+                            "features": [
+                                "http://jabber.org/protocol/disco#info",
+                                "http://jabber.org/protocol/disco#items",
+                                "urn:xmpp:ping",
+                            ],
+                        }
+                    }
+                )
+
+            elif "items" in data:
+                return jsonify(
+                    {"items": [{"jid": "example.org", "name": "Example Dot Org"}]}
+                )
+
+    return Response(status=501)
+
+
+if __name__ == "__main__":
+    app.run()
