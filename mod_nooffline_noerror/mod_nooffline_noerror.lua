9f62e7bc0c08b88fcc908b5f95c8fb15a8cb71e4
tmolitor <thilo@eightysoft.de>
2020-04-07 22:11:52 +0200

    mod_nooffline_noerror: Fix for missing log

--- mod_nooffline_noerror/mod_nooffline_noerror.lua
+++ mod_nooffline_noerror/mod_nooffline_noerror.lua
@@ -12,7 +12,9 @@ module:depends "mam";
 -- ignore offline messages and don't return any error (the message will be already in MAM at this point)
 -- this is *only* triggered if mod_offline is *not* loaded and completely ignored otherwise
 module:hook("message/offline/handle", function(event)
-	local log = event.origin and event.origin.log or module._log
-	log("info", "Ignoring offline message (mod_offline seems to be *not* loaded)...");
+	local log = event.origin and event.origin.log or module._log;
+	if log then
+		log("info", "Ignoring offline message (mod_offline seems *not* to be loaded)...");
+	end
 	return true;
 end, -100);
